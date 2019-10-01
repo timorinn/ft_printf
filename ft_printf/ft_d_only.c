09d8e0ad98421977767391b42e2b23f14b5e109e
Brittanie Ford <bford@at-i7.21-school.ru>
2019-10-01 19:12:46 +0300

    a

--- ft_printf/ft_d_only.c
+++ ft_printf/ft_d_only.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 10:42:59 by bford             #+#    #+#             */
-/*   Updated: 2019/10/01 18:24:49 by bford            ###   ########.fr       */
+/*   Updated: 2019/10/01 19:12:45 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int		ft_d_only(t_pf **l, long long d)
 {
 	int		sp_or_plus;
 	int		nol_or_min;
+	int		len;
 
+	len = ft_lennbr(d);
 	sp_or_plus = ((*l)->p ? '+' : 0);
 	sp_or_plus = ((*l)->s && !((*l)->p) ? ' ' : sp_or_plus);
 	nol_or_min = ((*l)->m ? '-' : 0);
@@ -28,10 +30,18 @@ int		ft_d_only(t_pf **l, long long d)
 		(*l)->i1 -= 1;
 	else if (sp_or_plus == '+' || d < 0)
 	{
+
+		//if (!((*l)->m) && (*l)->i1 - len > 0)
+		//	ft_many_write(nol_or_min, (*l)->i1 - len, l);
 		if (d < 0)
 			sp_or_plus = '-';
-		ft_many_write(sp_or_plus, 1, l);
 		(*l)->i1 -= 1;
+		if (!((*l)->m) && (*l)->i1 - len > 0)
+			ft_many_write(nol_or_min, (*l)->i1 - len, l);
+		ft_many_write(sp_or_plus, 1, l);
+		ft_putnbr_int(d, l);
+		if ((*l)->m && (*l)->i1 - len > 0)
+			ft_many_write(' ', (*l)->i1 - len, l);
 	}
-	return (ft_putnbr_int(d, l, sp_or_plus, nol_or_min));
+	return (1); // (ft_putnbr_int(d, l) , sp_or_plus, nol_or_min));
 }
--- ft_printf/ft_putnbr_int.c
+++ ft_printf/ft_putnbr_int.c
@@ -6,34 +6,32 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 17:14:08 by bford             #+#    #+#             */
-/*   Updated: 2019/10/01 18:25:54 by bford            ###   ########.fr       */
+/*   Updated: 2019/10/01 19:06:39 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "ft_printf.h"
 
-int		ft_putnbr_int(long long d, t_pf **l, char sp_pl, char min_nol)
+int		ft_putnbr_int(long long d, t_pf **l) //, char sp_pl, char min_nol)
 {
 	int			len;
 	long long	i;
 
-	sp_pl = 0;
-	min_nol = (min_nol == 0 ? ' ' : min_nol);
+	//sp_pl = 0;
+	//min_nol = (min_nol == 0 ? ' ' : min_nol);
 	d = (d < 0 ? -d : d);
 	len = ft_lennbr(d);
-	if (!((*l)->m) && (*l)->i1 - len > 0)
-		ft_many_write(min_nol, (*l)->i1 - len, l);
+	//if (!((*l)->m) && (*l)->i1 - len > 0)
+	//	ft_many_write(min_nol, (*l)->i1 - len, l);
 	i = (d > 9 ? ft_power(10, len - 1) : 1);
-	//printf("I = %lld\nD = %lld\nLen = %d\n", i, d, len);
 	while (i)
 	{
 		ft_many_write(d / i + '0', 1, l);
 		d %= i;
 		i /= 10;
 	}
-	//printf("i1 = %d | len = %d\n", (*l)->i1, len);
-	if ((*l)->m && (*l)->i1 - len > 0)
-		ft_many_write(' ', (*l)->i1 - len, l);
+	//if ((*l)->m && (*l)->i1 - len > 0)
+	//	ft_many_write(' ', (*l)->i1 - len, l);
 	return (1);
 }
