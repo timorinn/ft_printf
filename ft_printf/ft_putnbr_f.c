e7ab80e3bcd415d1a332a0b77cf52d4242e8a4c4
Brittanie Ford <bford@vo-j5.21-school.ru>
2019-10-03 20:36:34 +0300

    a

--- ft_printf/ft_putnbr_f.c
+++ ft_printf/ft_putnbr_f.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 19:58:36 by bford             #+#    #+#             */
-/*   Updated: 2019/10/03 20:33:44 by bford            ###   ########.fr       */
+/*   Updated: 2019/10/03 20:36:33 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_putnbr_f(long double second, t_pf **l, int second_len)
 	my = second / 10.0;
 	if (second_len--)
 		ft_putnbr_f(second / 10.0, l, second_len);
-	ft_many_write((int)(second % 10.0) + '0', 1, l);
+	//ft_many_write((int)(second % 10.0) + '0', 1, l);
 	l+=0;second_len+=0;
 	return (1);
 }
