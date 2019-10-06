9148ab634e948cc156b79705629dc58c19b7370a
Brittanie Ford <bford@vo-j6.21-school.ru>
2019-10-06 09:54:12 +0300

    a

--- ft_printf/ft_postwork.c
+++ ft_printf/ft_postwork.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 20:19:35 by bford             #+#    #+#             */
-/*   Updated: 2019/10/04 09:57:56 by bford            ###   ########.fr       */
+/*   Updated: 2019/10/06 09:45:06 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,16 @@ int		ft_postwork(t_pf **l, va_list a)
 {
 	char	spec;
 
+	a+=0;
 	spec = (*l)->c;
-	if ((!spec) || 
-	((spec == 'x' || spec == 'X') && ft_xX(l, a)) ||
+	if ((!spec))/*
+	||  ((spec == 'x' || spec == 'X') && ft_xX(l, a)) ||
 	((spec == 'd' || spec == 'i') && ft_d(l, a)) ||
-	(spec == 'c' && ft_c(l, a)) ||
-	(spec == 's' && ft_s(l, a)) ||
-	(spec == '%' && ft_pr(l)) ||
-	(spec == 'u' && ft_u(l, a)) ||
-	(spec == 'o' && ft_o(l, a)) ||
-	(spec == 'p' && ft_p(l, a)) ||
+	(spec == 'c' && ft_c(l, a)) || (spec == 's' && ft_s(l, a)) ||
+	(spec == '%' && ft_pr(l)) || (spec == 'u' && ft_u(l, a)) ||
+	(spec == 'o' && ft_o(l, a)) || (spec == 'p' && ft_p(l, a)) ||
 	(spec == 'f' && ft_f(l, a)))
+	*/
 		return (ft_nollist(l));
 	return (0);
 }
