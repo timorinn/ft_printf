7970d4eef8f799a0e643392400bda4350939bcb3
Brittanie Ford <bford@mi-h4.21-school.ru>
2019-10-06 17:57:23 +0300

    a

--- libft/libft.h
+++ libft/libft.h
@@ -6,19 +6,18 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 12:10:55 by bford             #+#    #+#             */
-/*   Updated: 2019/10/06 15:16:16 by bford            ###   ########.fr       */
+/*   Updated: 2019/10/06 17:56:56 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <string.h>
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <string.h>
+
 int		ft_putstr(const char *s);
 size_t	ft_strlen(const char *s);
 char	*ft_strnew(size_t size);
-char	*ft_strjoinfree(char *s1, const char *s2, int size);
 char	*ft_stradd(char **ms, char c, int size);
 char	*ft_strdup(char *src);
 int		ft_strdel(char **as);
